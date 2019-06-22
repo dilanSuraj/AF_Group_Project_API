f90d4fb8baaae255b7c0cd8215c1c605a5f5dfc2
dilanSuraj <suraj_amarasinghe@yahoo.com>
2019-06-23 00:12:41 +0530

    Enroll Course

--- server/routes/student.routes.js
+++ server/routes/student.routes.js
@@ -1,4 +1,5 @@
 const studentController = require('../controllers/student.controller');
+const courseController = require('../controllers/course.controller');
 const express = require('express');
 var router = express.Router();
 
@@ -29,11 +30,60 @@ router.route('/').get(function (req, res) {
     })
 });
 
+// router.route('/:email').get(function (req, res) {
+//     studentController.getByEmail(req.params.email).then(function (data) {
+//         console.log(data.data);
+//         res.status(data.status).send(data);
+//     }).catch(error => {
+//         res.status(error.status).send({
+//             message: error.message
+//         })
+//     })
+// });
+
 router.route('/:assignmentExamCode').get(function (req, res) {
     studentController.getByEmail(req.params.email).then(function (data) {
         res.status(data.status).send(
             data.data
         );
+    }).catch(error => {
+        res.status(error.status).send({
+            message: error.message
+        })
+    })
+});
+
+router.route('/unenrolled/:email').get(function (req, res) {
+    studentController.getByEmail(req.params.email).then(function (data) {
+        let enrolledCourses = data.data[0].courses;
+
+        courseController.getAcceptedCourses().then(function (data) {
+
+
+            let acceptedCourses = data.data;
+
+            let unenrolledCourses = [];
+
+            for(let acceptedCourse of acceptedCourses ){
+
+                console.log(acceptedCourse._id);
+                if(enrolledCourses.indexOf(acceptedCourse._id) > 0){
+
+                    unenrolledCourses.push(acceptedCourse);
+                }
+
+            }
+
+
+            res.status(data.status).send(
+                unenrolledCourses
+            );
+
+        }).catch(function (err) {
+            res.status(err.status).send({
+                message: err.message
+            })
+        });
     }).catch(error => {
         res.status(error.status).send({
             message: error.message
